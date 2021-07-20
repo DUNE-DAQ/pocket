0530a3531f5e6142cf9a3d56ff6140954f13be03
Bonnie King <misterbonnie@gmail.com>
2021-07-20 06:12:19 -0500

    fix run script

--- manifests/dunedaqers/run.sh
+++ manifests/dunedaqers/run.sh
@@ -15,24 +15,37 @@ echo $wdir
 
 kind create cluster --config ./ers-kind.config.yaml
 
-kubectl apply -f ../proxy-server.yaml
-sleep 4
-
 #namespaces
 kubectl apply -f ns-dunedaqers.yaml
 kubectl apply -f ns-kafka-kraft.yaml
 
+#use the regular Pocket proxy
+kubectl apply -f ../proxy-server.yaml
+
 #secrets
+pgpass="$(pwqgen)"
+pgpass="admin"
+echo $pgpass
+
 kubectl -n dunedaqers create secret generic postgres-secrets \
         --from-literal=POSTGRES_USER="admin" \
-        --from-literal=POSTGRES_PASSWORD="$(pwqgen)"\
-        --from-literal=DOTNETPOSTGRES_PASSWORD="Password= $POSTGRES_PASSWORD;"
+        --from-literal=POSTGRES_PASSWORD="$pgpass"
+
+# aspcore needs the password in a different format
+dotnetpass=`echo $pgpass| awk '{print "Password="$0";"}'`
+
+echo $dotnetpass
+
+kubectl -n dunedaqers create secret generic aspcore-secrets \
+	--from-literal=DOTNETPOSTGRES_PASSWORD="$dotnetpass"
+
 
 kubectl apply -f kafka.yaml
 
 #kubectl apply -f kafka-svc.yaml
-
 kubectl apply -f postgres.yaml
+kubectl apply -f postgres-svc.yaml
+
 kubectl apply -f aspcore.yaml
 
 echo "Proxy server available at: socks5 port 31000"
