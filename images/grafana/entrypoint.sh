fcb99162701f53580bf8f1b9b323efb5a07ecb93
Bonnie King <misterbonnie@gmail.com>
2021-08-18 15:29:29 -0500

    rely on env var for postgres password

--- images/grafana/entrypoint.sh
+++ images/grafana/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-sed -e "s+^  basicAuthPassword:.*+  basicAuthPassword: \"$POSTGRES_PASSWORD\"+" \
-/etc/grafana/provisioning/datasources/ers.yaml  > ers.yaml.updated \
-&& mv ers.yaml.updated /etc/grafana/provisioning/datasources/ers.yaml
-
 exec grafana-server
