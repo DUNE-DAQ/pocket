2849fdde755de55d79a5bbaebbe90977c53bfe05
Alessandro Thea <alessandro.thea@cern.ch>
2021-08-26 15:19:20 +0200

    playing with daq application docker images

--- images/daq_application_cvmfs/build.sh
+++ images/daq_application_cvmfs/build.sh
@@ -1,1 +1,8 @@
-docker build -v /cvmfs/dunedaq.opensciencegrid.org:/cvmfs/dunedaq.opensciencegrid.org  -v /cvmfs/dunedaq-development.opensciencegrid.org:/cvmfs/dunedaq-development.opensciencegrid.org --tag pocket-daq:0.1.1 daq_application_cvmfs/ 
+# Bash/Zsh independent way of determining the source path
+SH_SOURCE=${BASH_SOURCE[0]:-${(%):-%x}}
+HERE=$(cd $(dirname ${SH_SOURCE}) && pwd)
+
+TAG=pocket-daq-cvmfs
+VERSION=v0.1.0
+
+docker build -v /cvmfs/dunedaq.opensciencegrid.org:/cvmfs/dunedaq.opensciencegrid.org  -v /cvmfs/dunedaq-development.opensciencegrid.org:/cvmfs/dunedaq-development.opensciencegrid.org --tag $TAG:$VERSION $HERE 
