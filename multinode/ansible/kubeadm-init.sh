c3e657445770df16215f0897eba751483e928555
bonnieking <misterbnnie@gmail.com>
2022-02-23 20:06:47 +0100

    playbook/kube init/flannel manifest for multinode

--- multinode/ansible/kubeadm-init.sh
+++ multinode/ansible/kubeadm-init.sh
@@ -2,12 +2,8 @@
 set -o errexit -o nounset -o pipefail -x
 IFS=$'\n\t\v'
 
-LOAD_BALANCER_DNS=$1
-LOAD_BALANCER_PORT=6443
-
 rm -rf /var/lib/etcd/lost+found
-kubeadm init --control-plane-endpoint "$LOAD_BALANCER_DNS:$LOAD_BALANCER_PORT" \
-             --upload-certs \
+kubeadm init --upload-certs \
              --pod-network-cidr=192.168.66.0/8 \
              --ignore-preflight-errors=NumCPU
 
