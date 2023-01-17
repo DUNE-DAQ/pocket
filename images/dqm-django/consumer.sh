fc48608d705220870274aa95b99624147cae6fc6
Pat Riehecky <3534830+jcpunk@users.noreply.github.com>
2023-01-17 09:26:37 -0600

    dqm-django Containerfile now with DQM source (#69)

--- images/dqm-django/consumer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/src/dqm-backend/dqm/./prepare_dqm
-
-cd /src/dqm-backend/dqm/
-exec python3 dqm/manage.py shell < scripts/consumer.py
--- images/dqm-django/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/src/dqm-backend/dqm/./prepare_dqm
-
-cd /src/dqm-backend/dqm/dqm/
-exec /usr/local/bin/daphne dqm.asgi:application -b 0.0.0.0 -p 8000
