93fa7d2eda98d9323d7637a0a751b113ac11d31e
tta20 <tiagoalves@live.co.uk>
2023-11-15 18:43:58 +0100

    space was a mistake

--- .makefile/daq-kube.mk
+++ .makefile/daq-kube.mk
@@ -1,3 +1,3 @@
 ${MAKEFILE_DIR}/daq-kube/.kluctl.yaml:
 	@echo "Fetching daq-kube repo..."
-	@cd ${MAKEFILE_DIR} && git clone  https://github.com/DUNE-DAQ/daq-kube.git --recursive
+	@cd ${MAKEFILE_DIR} && git clone https://github.com/DUNE-DAQ/daq-kube.git --recursive
