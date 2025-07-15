cbb270b38c4eb4f4787a6730c3fcbe221507acb4
Pat Riehecky <riehecky@fnal.gov>
2025-07-15 08:53:34 -0500

    Also show urls used

--- .makefile/daq-kube.mk
+++ .makefile/daq-kube.mk
@@ -1,3 +1,4 @@
 ${MAKEFILE_DIR}/daq-kube/.kluctl.yaml:
 	@echo "Fetching daq-kube repo..."
+	@echo "  git clone https://github.com/DUNE-DAQ/daq-kube.git --recursive"
 	@cd ${MAKEFILE_DIR} && git clone https://github.com/DUNE-DAQ/daq-kube.git --recursive
--- .makefile/kind.mk
+++ .makefile/kind.mk
@@ -1,6 +1,7 @@
 ${MY_BINDIR}/kind-${KIND_VERSION}:
 	@mkdir -p ${MY_BINDIR}
 	@echo "Downloading kind ${KIND_VERSION} for ${OS} ${PLATFORM}..."
+	@echo "  https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${OS}-${PLATFORM}"
 	@curl -Lfs https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERSION}/kind-${OS}-${PLATFORM} -o ${MY_BINDIR}/kind-${KIND_VERSION}
 	@chmod +x ${MY_BINDIR}/kind-${KIND_VERSION}
 ${MY_BINDIR}/kind: ${MY_BINDIR}/kind-${KIND_VERSION}
