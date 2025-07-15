82e2fb469d9c9764f17023a7a972e132dad89d9d
Pat Riehecky <riehecky@fnal.gov>
2025-07-15 08:46:35 -0500

    Be more chatty about version/os/arch

--- .makefile/kluctl.mk
+++ .makefile/kluctl.mk
@@ -3,7 +3,7 @@ ${MY_BINDIR}/kluctl: test-jq-is-working
 	@echo ""
 	@echo -e "Finding latest version of \033[1mkluctl\033[0m..."
 	$(eval KLUCTL_VERSION=$(shell curl -Lfs https://api.github.com/repos/kluctl/kluctl/releases/latest | jq '.tag_name' | tr -d '"'))
-	@echo -e "Downloading \033[1mkluctl\033[0m..."
+	@echo -e "Downloading \033[1mkluctl\033[0m ${KLUCTL_VERSION} for ${OS} ${PLATFORM}..."
 	@curl -Lfs https://github.com/kluctl/kluctl/releases/download/${KLUCTL_VERSION}/kluctl_${KLUCTL_VERSION}_${OS}_${PLATFORM}.tar.gz -o ${MY_BINDIR}/kluctl-${KLUCTL_VERSION}.tar.gz
 	@cd ${MY_BINDIR} ; tar xf ${MY_BINDIR}/kluctl-${KLUCTL_VERSION}.tar.gz
 	@cd ${MY_BINDIR} ; mv kluctl kluctl-${KLUCTL_VERSION}
