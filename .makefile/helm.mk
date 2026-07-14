fe43a644089b6bdc1099bb04a3a55e4ed44f4b20
Pat Riehecky <riehecky@fnal.gov>
2026-07-14 08:02:58 -0500

    Fix `latest` version

--- .makefile/helm.mk
+++ .makefile/helm.mk
@@ -1,13 +1,14 @@
 ${MY_BINDIR}/helm: | test-jq-is-working
 	@mkdir -p ${MY_BINDIR}
 	@echo ""
-	@if [ "${HELM_VERSION}" = "latest" ]; then \
+	@HELM_VERSION="${HELM_VERSION}"; \
+	if [ "$${HELM_VERSION}" = "latest" ]; then \
 		HELM_VERSION=$$(curl -Lfs https://api.github.com/repos/helm/helm/releases/latest | jq '.tag_name' | tr -d '"'); \
-	fi
-	@echo -e "Downloading \033[1mhelm\033[0m ${HELM_VERSION} for ${OS} ${PLATFORM}..."
-	@echo "  https://get.helm.sh/helm-${HELM_VERSION}-${OS}-${PLATFORM}.tar.gz"
-	@curl -Lf https://get.helm.sh/helm-${HELM_VERSION}-${OS}-${PLATFORM}.tar.gz -o ${MY_BINDIR}/helm-${HELM_VERSION}.tar.gz
-	@cd ${MY_BINDIR} ; tar xf ${MY_BINDIR}/helm-${HELM_VERSION}.tar.gz
-	@cd ${MY_BINDIR} ; mv ${OS}-${PLATFORM}/helm helm-${HELM_VERSION}
-	@chmod +x ${MY_BINDIR}/helm-${HELM_VERSION}
-	@ln -sf ${MY_BINDIR}/helm-${HELM_VERSION} ${MY_BINDIR}/helm
+	fi; \
+	echo -e "Downloading \033[1mhelm\033[0m $${HELM_VERSION} for ${OS} ${PLATFORM}..."; \
+	echo "  https://get.helm.sh/helm-$${HELM_VERSION}-${OS}-${PLATFORM}.tar.gz"; \
+	curl -Lf https://get.helm.sh/helm-$${HELM_VERSION}-${OS}-${PLATFORM}.tar.gz -o ${MY_BINDIR}/helm-$${HELM_VERSION}.tar.gz; \
+	cd ${MY_BINDIR} && tar xf ${MY_BINDIR}/helm-$${HELM_VERSION}.tar.gz; \
+	cd ${MY_BINDIR} && mv ${OS}-${PLATFORM}/helm helm-$${HELM_VERSION}; \
+	chmod +x ${MY_BINDIR}/helm-$${HELM_VERSION}; \
+	ln -sf ${MY_BINDIR}/helm-$${HELM_VERSION} ${MY_BINDIR}/helm
--- .makefile/kluctl.mk
+++ .makefile/kluctl.mk
@@ -1,13 +1,14 @@
 ${MY_BINDIR}/kluctl: | test-jq-is-working
 	@mkdir -p ${MY_BINDIR}
 	@echo ""
-	@if [ "${KLUCTL_VERSION}" = "latest" ]; then \
+	@KLUCTL_VERSION="${KLUCTL_VERSION}"; \
+	if [ "$${KLUCTL_VERSION}" = "latest" ]; then \
 		KLUCTL_VERSION=$$(curl -Lfs https://api.github.com/repos/kluctl/kluctl/releases/latest | jq '.tag_name' | tr -d '"'); \
-	fi
-	@echo -e "Downloading \033[1mkluctl\033[0m ${KLUCTL_VERSION} for ${OS} ${PLATFORM}..."
-	@echo "  https://github.com/kluctl/kluctl/releases/download/${KLUCTL_VERSION}/kluctl_${KLUCTL_VERSION}_${OS}_${PLATFORM}.tar.gz"
-	@curl -Lf https://github.com/kluctl/kluctl/releases/download/${KLUCTL_VERSION}/kluctl_${KLUCTL_VERSION}_${OS}_${PLATFORM}.tar.gz -o ${MY_BINDIR}/kluctl-${KLUCTL_VERSION}.tar.gz
-	@cd ${MY_BINDIR} ; tar xf ${MY_BINDIR}/kluctl-${KLUCTL_VERSION}.tar.gz
-	@cd ${MY_BINDIR} ; mv kluctl kluctl-${KLUCTL_VERSION}
-	@chmod +x ${MY_BINDIR}/kluctl-${KLUCTL_VERSION}
-	@ln -sf ${MY_BINDIR}/kluctl-${KLUCTL_VERSION} ${MY_BINDIR}/kluctl
+	fi; \
+	echo -e "Downloading \033[1mkluctl\033[0m $${KLUCTL_VERSION} for ${OS} ${PLATFORM}..."; \
+	echo "  https://github.com/kluctl/kluctl/releases/download/$${KLUCTL_VERSION}/kluctl_$${KLUCTL_VERSION}_${OS}_${PLATFORM}.tar.gz"; \
+	curl -Lf https://github.com/kluctl/kluctl/releases/download/$${KLUCTL_VERSION}/kluctl_$${KLUCTL_VERSION}_${OS}_${PLATFORM}.tar.gz -o ${MY_BINDIR}/kluctl-$${KLUCTL_VERSION}.tar.gz; \
+	cd ${MY_BINDIR} && tar xf ${MY_BINDIR}/kluctl-$${KLUCTL_VERSION}.tar.gz; \
+	cd ${MY_BINDIR} && mv kluctl kluctl-$${KLUCTL_VERSION}; \
+	chmod +x ${MY_BINDIR}/kluctl-$${KLUCTL_VERSION}; \
+	ln -sf ${MY_BINDIR}/kluctl-$${KLUCTL_VERSION} ${MY_BINDIR}/kluctl
